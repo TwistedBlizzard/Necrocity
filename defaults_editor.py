061b1c968a43231ea18c882b8a13b27d63dfc934
TwistedBlizzard <twistedblizzard@live.com>
2018-11-15 23:17:01 +0000

    Added Defaults Editor
    Added a tool for editing the default associations of a race.

---
+++ defaults_editor.py
@@ -0,0 +1,113 @@
+import os, json
+
+class Next(Exception):
+    pass
+
+class Skip(Exception):
+    pass
+
+class Stop(Exception):
+    pass
+
+def load(race):
+    data = {}
+    path = str(os.path.join('res', race.lower() + '_associations.json'))
+    with open(path, 'r') as json_file:
+        data = json.load(json_file)
+    return data
+
+def save(race, data):
+    path = str(os.path.join('res', race.lower() + '_associations.json'))
+    with open(path, 'w') as json_file:
+        json.dump(data, json_file)
+
+def edit(associations):
+    try:
+        for root in associations:
+            print('Please enter words relating to the word %s:' % (root))
+            print("Enter 'next' to go to the next word.")
+            print("Enter 'skip' to skip to words that have no associations.")
+            print("Enter 'stop' to stop editing.")
+            if len(associations[root]) > 0:
+                print('Current Associations:')
+                for association in associations[root]:
+                    print(' ', association)
+            else:
+                print('There are no current associations.')
+            print('New Associations:')
+            try:
+                while True:
+                    association = input('  ')
+                    if association == '':
+                        continue
+                    elif association == 'next':
+                        raise Next()
+                    elif association == 'skip':
+                        raise Skip()
+                    elif association == 'stop':
+                        raise Stop()
+                    else:
+                        associations[root].append(association)
+            except Next:
+                continue
+            except Skip:
+                break
+        new_words = []
+        for root in associations:
+            for association in associations[root]:
+                if association not in new_words and association not in associations:
+                    new_words.append(association)
+        for root in new_words:
+            associations[root] = []
+            print('Please enter words relating to the word %s:' % (root))
+            print("Enter 'next' to go to the next word.")
+            print("Enter 'skip' to skip to adding new words.")
+            print("Enter 'stop' to stop editing.")
+            print('There are no current associations')
+            try:
+                while True:
+                    association = input('  ')
+                    if association == '':
+                        continue
+                    elif association == 'next':
+                        raise Next()
+                    elif association == 'skip':
+                        raise Skip()
+                    elif association == 'stop':
+                        raise Stop()
+                    else:
+                        associations[root].append(association)
+            except Next:
+                continue
+            except Skip:
+                break
+        while True:
+            print('Please enter a word to add to associations:' % (root))
+            print("Enter 'next' to go to the next word.")
+            print("Enter 'stop' to stop editing.")
+            root = input()
+            if root not in associations:
+                associations[root] = []
+            else:
+                continue
+            try:
+                while True:
+                    association = input('  ')
+                    if association == '':
+                        continue
+                    elif association == 'next':
+                        raise Next()
+                    elif association == 'stop':
+                        raise Stop()
+                    else:
+                        associations[root].append(association)
+            except Next:
+                continue
+    except Stop:
+        pass
+    return associations
+
+if __name__ == '__main__':
+    associations = load('Human')
+    associations = edit(associations)
+    save('Human', associations)
